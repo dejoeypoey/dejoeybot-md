7d54051b91956ef6378f3c1821260350dcb080b1
dejoeypoey <111537752+dejoeypoey@users.noreply.github.com>
2022-08-21 22:55:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
    * Create By Dika Ardnt.
    * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Follow https://github.com/dejoeypoey/
 */
 
 const fs = require('fs')
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['6281390260036']
+global.premium = ['6281390260036']
+global.packname = 'De Joey'
+global.author = 'De Joey Bot'
+global.sessionName = 'dejoeybot'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
@@ -38,10 +38,10 @@ global.mess = {
 }
 global.limitawal = {
     premium: "Infinity",
-    free: 100
+    free: 20
 }
 global.thumb = fs.readFileSync('./lib/hisoka.jpg')
-global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+global.visoka = { url: 'https://telegra.ph/file/bb15e558687db4a54bf92.jpg' }
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
